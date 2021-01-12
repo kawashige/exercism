7102a8f130f3772dbb0fc4afc693b42f2d968f4e
Takahiro Kawashige <lapavoni@gmail.com>
2021-01-12 20:17:00 +0900

    Refactor anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -7,14 +7,20 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
 
     possible_anagrams
         .iter()
-        .filter(|p| p.len() == word.len())
         .filter(|p| {
-            let p_lowercase = p.to_lowercase();
-            let mut possible_chars = p_lowercase.chars().collect::<Vec<char>>();
-            possible_chars.sort_unstable();
-            p_lowercase != word_lowercase
-                && (0..possible_chars.len()).all(|i| possible_chars[i] == word_chars[i])
+            if p.len() != word.len() {
+                false
+            } else {
+                let p_lowercase = p.to_lowercase();
+                if p_lowercase == word_lowercase {
+                    false
+                } else {
+                    let mut possible_chars = p_lowercase.chars().collect::<Vec<char>>();
+                    possible_chars.sort_unstable();
+                    possible_chars == word_chars
+                }
+            }
         })
-        .map(|p| *p)
+        .cloned()
         .collect()
 }
