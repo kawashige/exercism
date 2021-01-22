265ae8c8b197b932e78fd81e2acb0641ca6c3c35
Takahiro Kawashige <lapavoni@gmail.com>
2021-01-22 18:48:03 +0900

    Refactor luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,30 +1,23 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if let Some(input) = code
-        .replace(" ", "")
-        .chars()
-        .map(|c| c.to_digit(10))
-        .collect::<Option<Vec<u32>>>()
-    {
-        1 < input.len()
-            && input
-                .into_iter()
-                .rev()
-                .zip(1..)
-                .map(|(n, i)| {
-                    if i % 2 == 0 {
-                        if 4 < n {
-                            n * 2 - 9
-                        } else {
-                            n * 2
-                        }
-                    } else {
-                        n
-                    }
-                })
-                .sum::<u32>()
-                % 10
-                == 0
+    let input = code.replace(" ", "");
+    if input.len() < 2 {
+        return false;
+    }
+    if let Some(n) = input.chars().rev().enumerate().try_fold(0, |acc, (i, c)| {
+        let n = c.to_digit(10)?;
+        (acc + if i % 2 == 0 {
+            n
+        } else {
+            if 4 < n {
+                n * 2 - 9
+            } else {
+                n * 2
+            }
+        })
+        .into()
+    }) {
+        n % 10 == 0
     } else {
         false
     }
