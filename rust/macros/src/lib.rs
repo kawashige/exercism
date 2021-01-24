c5aaea266aca4888528e5b1b77a3608f8e4865e3
Takahiro Kawashige <lapavoni@gmail.com>
2021-01-24 21:24:39 +0900

    Refactor macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,13 +1,10 @@
 #[macro_export]
 macro_rules! hashmap {
-    ($($key:literal => $val:expr),+,) => {
-        $crate::hashmap!($($key => $val),*)
-    };
-    ($($key:literal => $val:expr),*) => {{
+    ($($($key:literal => $val:expr)+$(,)?)*) => {{
         let mut hm = ::std::collections::HashMap::new();
-        $(
+        $($(
             hm.insert($key, $val);
-        )*
+        )+)*
         hm
     }};
 }
