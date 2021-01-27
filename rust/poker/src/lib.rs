89354b66ebe87bf895391665f7ae34e10fd4556a
Takahiro Kawashige <lapavoni@gmail.com>
2021-01-27 20:28:21 +0900

    Refactor porker

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -7,26 +7,39 @@ use std::collections::HashMap;
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 #[derive(PartialEq)]
 struct Hand<'a> {
-    ordered_nums: [usize; 5],
-    is_flash: bool,
+    score: Score,
     src: &'a str,
+}
+#[derive(Ord, Eq, PartialEq, PartialOrd, Debug)]
+enum Score {
+    RoyalStraightFlush,
+    StraightFlush(u8),
+    FourCard(u8, u8),
+    FullHouse(u8, u8),
+    Flash(u8, u8, u8, u8, u8),
+    Straight(u8),
+    FiveHighStraight,
+    ThreeCards(u8, u8, u8),
+    TwoPairs(u8, u8, u8),
+    OnePair(u8, u8, u8, u8),
+    NoHand(u8, u8, u8, u8, u8),
 }
 
 impl<'a> From<&'a str> for Hand<'a> {
     fn from(src: &'a str) -> Self {
-        let mut v: Vec<(usize, char)> = src
+        let mut v: Vec<(u8, char)> = src
             .split_whitespace()
             .take(5)
             .map(|src| {
                 let (num, mark) = src.split_at(src.len() - 1);
                 (
                     match num {
-                        "A" => 14,
-                        "J" => 11,
-                        "Q" => 12,
-                        "K" => 13,
-                        _ => num.parse().unwrap(),
-                    },
+                        "A" => 0,
+                        "K" => 1,
+                        "Q" => 2,
+                        "J" => 3,
+                        _ => 14 - num.parse::<u8>().unwrap(),
+                    } as u8,
                     mark.chars().next().unwrap(),
                 )
             })
@@ -36,13 +49,12 @@ impl<'a> From<&'a str> for Hand<'a> {
             *map.entry(c.0).or_insert(0) += 1;
             map
         });
-        v.sort_by_key(|c| (-counts[&c.0], -(c.0 as i32)));
+        v.sort_by_key(|c| (-counts[&c.0], c.0));
 
-        let is_flash = (1..v.len()).all(|i| v[0].1 == v[i].1);
+        let is_flush = (1..v.len()).all(|i| v[0].1 == v[i].1);
 
         Self {
-            ordered_nums: [v[0].0, v[1].0, v[2].0, v[3].0, v[4].0],
-            is_flash,
+            score: Self::score([v[0].0, v[1].0, v[2].0, v[3].0, v[4].0], is_flush),
             src,
         }
     }
@@ -50,44 +62,40 @@ impl<'a> From<&'a str> for Hand<'a> {
 
 impl<'a> PartialOrd for Hand<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(
-            self.hand()
-                .cmp(&other.hand())
-                .then(other.ordered_nums.cmp(&self.ordered_nums)),
-        )
+        Some(self.score.cmp(&other.score))
     }
 }
 
 impl<'a> Hand<'a> {
-    fn hand(&'a self) -> usize {
-        match (self.ordered_nums, self.is_flash) {
-            // Royal Straight Flash
-            (v, true) if Self::is_straight(v) && v[0] == 14 => 1,
-            // Straight Falsh
-            (v, true) if Self::is_straight(v) => 2,
+    pub fn score(nums: [u8; 5], is_flush: bool) -> Score {
+        match (nums, is_flush) {
+            // Royal Straight Flush
+            (v, true) if Self::is_straight(v) && v[0] == 14 => Score::RoyalStraightFlush,
+            // Straight Flush
+            (v, true) if Self::is_straight(v) => Score::StraightFlush(v[0]),
             // Four Cards
-            (v, _) if v[0] == v[1] && v[0] == v[2] && v[0] == v[3] => 3,
+            (v, _) if v[0] == v[1] && v[0] == v[2] && v[0] == v[3] => Score::FourCard(v[0], v[4]),
             // Full House
-            (v, _) if v[0] == v[1] && v[0] == v[2] && v[3] == v[4] => 4,
+            (v, _) if v[0] == v[1] && v[0] == v[2] && v[3] == v[4] => Score::FullHouse(v[0], v[3]),
             // Flash
-            (_, true) => 5,
+            (v, true) => Score::Flash(v[0], v[1], v[2], v[3], v[4]),
             // 5-high Straight
-            (v, _) if Self::is_straight(v) && v[0] == 14 && v[1] == 5 => 7,
+            (v, _) if Self::is_straight(v) && v[0] == 0 && v[1] == 9 => Score::FiveHighStraight,
             // Straight
-            (v, _) if Self::is_straight(v) => 6,
+            (v, _) if Self::is_straight(v) => Score::Straight(v[0]),
             // Three Cards
-            (v, _) if v[0] == v[1] && v[0] == v[2] => 8,
+            (v, _) if v[0] == v[1] && v[0] == v[2] => Score::ThreeCards(v[0], v[3], v[4]),
             // Two Pair
-            (v, _) if v[0] == v[1] && v[2] == v[3] => 9,
+            (v, _) if v[0] == v[1] && v[2] == v[3] => Score::TwoPairs(v[0], v[2], v[4]),
             // One Pair
-            (v, _) if v[0] == v[1] => 10,
+            (v, _) if v[0] == v[1] => Score::OnePair(v[0], v[2], v[3], v[4]),
             // No hand
-            _ => 11,
+            _ => Score::NoHand(nums[0], nums[1], nums[2], nums[3], nums[4]),
         }
     }
 
-    fn is_straight(nums: [usize; 5]) -> bool {
-        nums == [14, 5, 4, 3, 2] || (1..5).all(|i| nums[0] == nums[i] + i)
+    fn is_straight(nums: [u8; 5]) -> bool {
+        nums == [0, 9, 10, 11, 12] || (1..5).all(|i| nums[i] == nums[0] + i as u8)
     }
 }
 
